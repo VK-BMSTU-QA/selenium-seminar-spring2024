5967864af22c839de54a0bf3d312994f6db4ee95
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2024-03-31 18:29:30 +0300

    небольшие изменения

--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -37,7 +37,7 @@ class LoginPage(BasePage):
     url = 'https://park.vk.company/'
 
     def login(self, user, password):
-        self.click(LoginPageLocators.LOGIN_LOCATOR)
+        self.click(LoginPageLocators.LOGIN_BUTTON)
         self.find(LoginPageLocators.LOGIN).send_keys(user)
         self.find(LoginPageLocators.PASSWORD).send_keys(password)
         self.click(LoginPageLocators.SUBMIT)
--- Selenium/code/ui/locators/park_locators.py
+++ Selenium/code/ui/locators/park_locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    LOGIN_LOCATOR = (By.LINK_TEXT, 'Войти')
+    LOGIN_BUTTON = (By.LINK_TEXT, 'Войти')
     LOGIN = (By.NAME, 'login')
     PASSWORD = (By.NAME, 'password')
     SUBMIT = (By.ID, 'popup-login-form-submit')
