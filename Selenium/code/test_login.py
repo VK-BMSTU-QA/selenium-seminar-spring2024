67adeb6076e659ccf3d6b04fb72a1c3b26a02136
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2024-03-31 23:39:42 +0300

    review fix

--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -3,23 +3,12 @@ import time
 import pytest
 from _pytest.fixtures import FixtureRequest
 
-from ui.locators.park_locators import LoginPageLocators, HeaderLocators, SettingsLocators
+from ui.locators.park_locators import (
+    LoginPageLocators,
+    HeaderLocators,
+    SettingsLocators,
+)
 from ui.pages.base_page import BasePage
-
-
-class BaseCase:
-    authorize = True
-
-    @pytest.fixture(scope='function', autouse=True)
-    def setup(self, driver, config, request: FixtureRequest):
-        self.driver = driver
-        self.config = config
-
-        self.login_page = LoginPage(driver)
-        if self.authorize:
-            print('Do something for login')
-            credentials = request.getfixturevalue('credentials')
-            self.main_page = self.login_page.login(**credentials)
 
 
 @pytest.fixture(scope='session')
@@ -28,20 +17,40 @@ def credentials():
         return json.load(file)
 
 
-@pytest.fixture(scope='session')
-def cookies(credentials, config):
-    pass
+class BaseCase:
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, driver, config, request: FixtureRequest):
+        self.driver = driver
+        self.config = config
+
+        self.login_page = LoginPage(driver)
+        self.login_setup(request)
+
+    def login_setup(self, request):
+        pass
+
+
+class LogedCase(BaseCase):
+    def login_setup(self, request):
+        credentials = request.getfixturevalue('credentials')
+        self.main_page = self.login_page.login(**credentials)
 
 
 class LoginPage(BasePage):
     url = 'https://park.vk.company/'
 
     def login(self, user, password):
-        self.click(LoginPageLocators.LOGIN_BUTTON)
-        self.find(LoginPageLocators.LOGIN).send_keys(user)
-        self.find(LoginPageLocators.PASSWORD).send_keys(password)
-        self.click(LoginPageLocators.SUBMIT)
-        time.sleep(3)
+        self.click(LoginPageLocators.LOGIN_BUTTON, timeout=5)
+
+        login_input = self.find(LoginPageLocators.LOGIN, timeout=5)
+        login_input.clear()
+        login_input.send_keys(user)
+
+        password_input = self.find(LoginPageLocators.PASSWORD, timeout=5)
+        password_input.clear()
+        password_input.send_keys(password)
+
+        self.click(LoginPageLocators.SUBMIT, timeout=5)
         return MainPage(self.driver)
 
 
@@ -53,24 +62,22 @@ class MainPage(BasePage):
         self.go_to_section(section_locator2)
 
     def go_to_section(self, section_locator):
-        self.find(section_locator).click()
-        time.sleep(3)
+        self.find(section_locator, timeout=5).click()
 
     def update_profile_about(self, new_info: str) -> str:
-        text_area = self.find(SettingsLocators.PROFILE_ABOUT)
+        text_area = self.find(SettingsLocators.PROFILE_ABOUT, timeout=5)
         old_info = text_area.text
         text_area.clear()
         text_area.send_keys(new_info)
-        self.find(SettingsLocators.SUBMIT).click()
-        time.sleep(3)
+        self.find(SettingsLocators.SUBMIT, timeout=5).click()
         return old_info
 
 
 class TestLogin(BaseCase):
     authorize = True
 
-    # @pytest.mark.skip('skip')
     def test_login(self, credentials):
+        main_page = self.login_page.login(**credentials)
         assert 'Блоги' in self.driver.page_source
         assert 'Люди' in self.driver.page_source
         assert 'Программа' in self.driver.page_source
@@ -79,33 +86,46 @@ class TestLogin(BaseCase):
         assert 'Вакансии' in self.driver.page_source
 
 
-class TestLK(BaseCase):
+class TestLK(LogedCase):
+    @pytest.mark.parametrize(
+        'locator_from,expected_from,locator_to,expected_to',
+        [
+            pytest.param(
+                HeaderLocators.BLOGS, ['Все блоги', 'Прямой эфир'],
+                HeaderLocators.PEOPLE, [
+                    'Сообщество проекта', 'Статистика', 'Фильтры'
+                ],
+            ),
+            pytest.param(
+                HeaderLocators.PROGRAM, [
+                    'Мои учебные программы',
+                    'Основные программы',
+                    'Открытые курсы',
+                    'Архив видео',
+                ],
+                HeaderLocators.GRADUATES, [
+                    'Наши выпускники',
+                    'Осень 2023',
+                    'Весна 2023',
+                ],
+            ),
+        ]
+    )
+    def test_switch_sections(
+        self,
+        locator_from,
+        expected_from,
+        locator_to,
+        expected_to,
+    ):
+        self.main_page.go_to_section(locator_from)
+        for item in expected_from:
+            assert item in self.driver.page_source
 
-    # @pytest.mark.skip('skip')
-    def test_switch_blogs_to_people(self):
-        self.main_page.go_to_section(HeaderLocators.BLOGS)
-        assert 'Все блоги' in self.driver.page_source
-        assert 'Прямой эфир' in self.driver.page_source
+        self.main_page.go_to_section(locator_to)
+        for item in expected_to:
+            assert item in self.driver.page_source
 
-        self.main_page.go_to_section(HeaderLocators.PEOPLE)
-        assert 'Сообщество проекта' in self.driver.page_source
-        assert 'Статистика' in self.driver.page_source
-        assert 'Фильтры' in self.driver.page_source
-
-    # @pytest.mark.skip('skip')
-    def test_switch_program_to_graduates(self):
-        self.main_page.go_to_section(HeaderLocators.PROGRAM)
-        assert 'Мои учебные программы' in self.driver.page_source
-        assert 'Основные программы' in self.driver.page_source
-        assert 'Открытые курсы' in self.driver.page_source
-        assert 'Архив видео' in self.driver.page_source
-
-        self.main_page.go_to_section(HeaderLocators.GRADUATES)
-        assert 'Наши выпускники' in self.driver.page_source
-        assert 'Осень 2023' in self.driver.page_source
-        assert 'Весна 2023' in self.driver.page_source
-
-    # @pytest.mark.skip('skip')
     def test_change_profile_about(self):
         self.driver.get('https://park.vk.company/cabinet/settings/')
         assert 'О себе' in self.driver.page_source
