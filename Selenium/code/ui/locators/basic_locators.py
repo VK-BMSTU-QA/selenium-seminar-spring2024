5509bfd8d055c66a9cdfb56bfc3151497ebe0615
Belozerovmsk <belozerov.msk@gmail.com>
2024-03-31 21:02:02 +0300

    tests are done

--- Selenium/code/ui/locators/basic_locators.py
+++ Selenium/code/ui/locators/basic_locators.py
@@ -20,3 +20,15 @@ class MainPageLocators(BasePageLocators):
 
 class EventsPageLocators(BasePageLocators):
     pass
+
+
+class LocatorsForParkPages:
+    LOGIN_BUTTON = (By.CSS_SELECTOR, '.nav__button__menu>a')
+    USER_FIELD = (By.CSS_SELECTOR, 'input[placeholder="Логин или почта"]')
+    PASSWORD_FIELD = (By.CSS_SELECTOR, 'input[placeholder="Пароль"]')
+    ABOUT_INPUT = (By.ID, 'profile_about')
+    ENG_NAME_INPUT = (By.ID, 'id_first_name_en')
+    ENG_SURNAME_INPUT = (By.ID, 'id_last_name_en')
+    SETTINGS_LINK = (By.XPATH, '//*[@id="wrapper"]/div[2]/a')
+    SUBMIT_BUTTON = (By.XPATH, '//*[@id="content"]/div/div[1]/form/div[2]/button')
+    MENU_ITEM_LINK = lambda item_name: (By.LINK_TEXT, item_name)
