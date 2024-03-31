a76acce111e6f4a201d1306462dc0e9781615b21
vilinvil <ivn15072002@gmail.com>
2024-03-31 15:04:49 +0300

    added Credentials class and comment how start in README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # selenium-seminar-spring2024
+
+тг для связи - 
+
+### Запуск
+ pytest test_login.py --url="https://park.vk.company" --login="your_login" --password="your_password"
--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -7,24 +7,24 @@ from ui.locators import basic_locators
 
 
 class BaseCase:
-    authorize = True
-
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, driver, config, request: FixtureRequest):
         self.driver = driver
         self.config = config
 
         self.login_page = LoginPage(driver)
-        if self.authorize:
-            print('Do something for login')
 
+class Credentials:
+     def __init__(self, login, password):
+          self.login = login
+          self.password = password
 
 @pytest.fixture(scope='session')
 def credentials(request):
         login = request.config.getoption('--login')
         password = request.config.getoption('--password')
         
-        return {"login":login, "password":password}
+        return Credentials(login, password)
 
 
 @pytest.fixture(scope='session')
@@ -36,15 +36,14 @@ class LoginPage(BasePage):
     url = 'https://park.vk.company/'
     locators = basic_locators.TechnoParkLocators()
 
-    def login(self, user, password):
-        div_login = self.find(self.locators.DIV_LOGIN_LOCATOR, timeout=5)
-        div_login.click()
+    def login(self, credentials: Credentials):
+        self.click(self.locators.DIV_LOGIN_LOCATOR, timeout=5)
 
         login_input = self.find(self.locators.LOGIN_INPUT_LOCATOR, timeout=5)
-        login_input.send_keys(user)
+        login_input.send_keys(credentials.login)
 
         password_input = self.find(self.locators.PASSWORD_INPUT_LOCATOR, timeout=5)
-        password_input.send_keys(password)
+        password_input.send_keys(credentials.password)
 
         self.click(self.locators.INPUT_BUTTON_IN_LOGIN_LOCATOR, timeout=5)
 
@@ -59,7 +58,7 @@ class TestLogin(BaseCase):
     authorize = True
 
     def test_login(self, credentials):
-        main_page = self.login_page.login(credentials["login"], credentials["password"])
+        main_page = self.login_page.login(credentials)
         assert isinstance(main_page, MainPage)
 
 
