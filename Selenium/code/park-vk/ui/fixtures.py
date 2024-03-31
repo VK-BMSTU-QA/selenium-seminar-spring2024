5c2e35580a096a72046f392682c9d8520769c93c
Maxim Davydov <norm.pochta.maxa@gmail.com>
2024-03-31 16:38:26 +0300

    Add base page class, fixtures and conftest.py

---
+++ Selenium/code/park-vk/ui/fixtures.py
@@ -0,0 +1,62 @@
+import pytest
+
+from os import environ
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from ui.pages.login_page import LoginPage
+from ui.pages.main_page import MainPage
+
+
+@pytest.fixture()
+def driver(config):
+    browser = config['browser']
+    url = config['url']
+    selenoid = config['selenoid']
+    vnc = config['vnc']
+    options = Options()
+    if selenoid:
+        capabilities = {
+            'browserName': 'chrome',
+            'version': '118.0',
+        }
+        if vnc:
+            capabilities['enableVNC'] = True
+        driver = webdriver.Remote(
+            'http://127.0.0.1:4444/wd/hub',
+            options=options,
+            desired_capabilities=capabilities
+        )
+    elif browser == 'chrome':
+        driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    elif browser == 'firefox':
+        driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    else:
+        raise RuntimeError(f'Unsupported browser: "{browser}"')
+    driver.get(url)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+def get_driver(browser_name):
+    if browser_name == 'chrome':
+        browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    elif browser_name == 'firefox':
+        browser = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    else:
+        raise RuntimeError(f'Unsupported browser: "{browser_name}"')
+    browser.maximize_window()
+    return browser
+
+
+@pytest.fixture(scope='session')
+def credentials():
+    return (environ.get("PARK_VK_LOGIN", "Test"), environ.get("PARK_VK_PASSWORD", "Test"))
+
+
+@pytest.fixture(scope='session')
+def cookies(credentials, login_page):
+    pass
