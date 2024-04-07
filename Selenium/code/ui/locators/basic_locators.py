04e41872718ac198452f62229aaabbc04b6d53a3
Владимир Захаров <vl.zakharov@vk.team>
2024-04-07 19:09:25 +0300

    Fix selectors

--- Selenium/code/ui/locators/basic_locators.py
+++ Selenium/code/ui/locators/basic_locators.py
@@ -30,22 +30,22 @@ class BaseParkPageLocators:
     LOGO_IMG = (By.CSS_SELECTOR, '#header img')
 
 class MainParkPageLocators(BaseParkPageLocators):
-    BLOGS_BUTTON = (By.XPATH, '//*[@id="header"]//a[@href="/blog/"]')
-    BLOGS_CONTENT_TITLE = (By.XPATH, '//*[@id="content"]//h2')
+    BLOGS_BUTTON = (By.CSS_SELECTOR, '#header a[href="/blog/"]'
+    BLOGS_CONTENT_TITLE = (By.CSS_SELECTOR, '#content h2')
 
-    PEOPLE_BUTTON = (By.XPATH, '//*[@id="header"]//a[@href="/people/"]')
-    PEOPLE_CONTENT_TITLE = (By.XPATH, '//*[@id="content"]//h2')
+    PEOPLE_BUTTON = (By.CSS_SELECTOR, '#header a[href="/people/"]')
+    PEOPLE_CONTENT_TITLE = (By.CSS_SELECTOR, '#content h2')
 
-    PROGRAM_BUTTON = (By.XPATH, '//*[@id="header"]//a[@href="/curriculum/program/"]')
+    PROGRAM_BUTTON = (By.CSS_SELECTOR, '#header a[href="/curriculum/program/"]')
 
-    ALUMNI_BUTTON = (By.XPATH, '//*[@id="header"]//a[@href="/alumni/"]')
-    ALUMNI_CONTENT_TITLE = (By.XPATH, '//*[@id="content"]//h1')
+    ALUMNI_BUTTON = (By.CSS_SELECTOR, '#header a[href="/alumni/"]')
+    ALUMNI_CONTENT_TITLE = (By.CSS_SELECTOR, '#content h1')
 
-    MAIN_PROGRAMS_TITLE = (By.XPATH, '//*[@id="content"]//a[@href="/curriculum/program/main/"]')
+    MAIN_PROGRAMS_TITLE = (By.CSS_SELECTOR, '#content a[href="/curriculum/program/main/"]')
 
     USER_DROPDOWN = (By.ID, 'dropdown-user-trigger')
-    USER_MENU_SETTINGS_BUTTON = (By.XPATH, '//*[@id="dropdown-user-menu"]//li[@class="item-settings"]')
+    USER_MENU_SETTINGS_BUTTON = (By.CSS_SELECTOR, '#dropdown-user-menu li.item-settings')
     PROFILE_ABOUT = (By.ID, 'profile_about')
 
     SETTINGS_SUBMIT_BUTTON = (By.NAME, 'submit_profile_edit')
-    SETTINGS_SUCCESS_CHANGE_MSG = (By.XPATH, '//*[@id="content"]//div[@class="profile_settings_success-msg"]')
+    SETTINGS_SUCCESS_CHANGE_MSG = (By.CSS_SELECTOR, '#content div.profile_settings_success-msg')
